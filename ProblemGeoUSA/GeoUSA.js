6baac6bb9f4631d9d65f0a6d9ec5f19f059fb08b
Chris Gerber <chris.gerber@tapjoy.com>
2014-03-16 21:11:50 -0400

    Finish Subtask 1a

--- ProblemGeoUSA/GeoUSA.js
+++ ProblemGeoUSA/GeoUSA.js
@@ -7,6 +7,7 @@ var margin = {
 
 var width = 1060 - margin.left - margin.right;
 var height = 800 - margin.bottom - margin.top;
+var centered;
 
 var bbVis = {
   x: 100,
@@ -33,11 +34,26 @@ var projection = d3.geo.albersUsa().translate([width / 2, height / 2]);//.precis
 var path = d3.geo.path().projection(projection);
 
 var dataSet = {};
-
+var stations = [];
 
 function loadStations() {
   d3.csv("../data/NSRDB_StationsMeta.csv",function(error,data){
-    //....
+    data.forEach(function(d) {
+      var station = {
+        id:   d["USAF"],
+        name: d["STATION"],
+        lon:  parseFloat(d["NSRDB_LON(dd)"]),
+        lat:  parseFloat(d["NSRDB_LAT (dd)"])
+      };
+      var coord = projection([station.lon, station.lat]);
+      if (coord) {
+        station.x = coord[0];
+        station.y = coord[1];
+        stations.push(station);
+      }
+    });
+
+    drawStations();
   });
 }
 
@@ -54,30 +70,64 @@ function loadStats() {
 d3.json("../data/us-named.json", function(error, data) {
   var usMap = topojson.feature(data,data.objects.states).features
 
-  // see also: http://bl.ocks.org/mbostock/4122298
   svg.selectAll(".country")
     .data(usMap)
     .enter().append("path")
-    .attr("d", path);
+    .attr("class", "country")
+    .attr("d", path)
+    .on("click", stateClicked);
 
   loadStats();
 });
 
+var drawStations = function() {
+  svg.selectAll(".station")
+    .data(stations)
+    .enter().append("circle")
+    .attr({
+      class: "station",
+      cx: function(d) { return d.x; },
+      cy: function(d) { return d.y; },
+      r: 2
+    });
+}
+
 // ALL THESE FUNCTIONS are just a RECOMMENDATION !!!!
-var createDetailVis = function(){
+var createDetailVis = function() {
 
 }
 
-var updateDetailVis = function(data, name){
+var updateDetailVis = function(data, name) {
 
 }
 
 // ZOOMING
+function stateClicked(d) {
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    centered = d;
+    doZoom(centroid[0], centroid[1], 4);
+  } else {
+    resetZoom();
+  }
+}
+
+function doZoom(x, y, k) {
+  svg.selectAll("path")
+    .classed("active", centered && function(d) { return d === centered; });
+
+  svg.transition()
+    .duration(750)
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+    .style("stroke-width", 1.5 / k + "px");
+}
+
 function zoomToBB() {
 
 }
 
 function resetZoom() {
-
+  centered = null;
+  doZoom(width / 2, height / 2, 1);
 }
 

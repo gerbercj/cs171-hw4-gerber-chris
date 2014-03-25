88c80ca461baf130b29e22b7a26191505b7fa67e
Chris Gerber <chris.gerber@tapjoy.com>
2014-03-24 23:30:33 -0400

    Add details to right of map

--- ProblemGeoWorld/GeoWorld.js
+++ ProblemGeoWorld/GeoWorld.js
@@ -21,6 +21,8 @@ var svg = d3.select("#vis").append("svg").attr({
 }).append("g").attr({
   transform: "translate(" + margin.left + "," + margin.top + ")"
 });
+var legend = svg.append("g");
+var textLabel = d3.select("#textLabel");
 
 var projectionMethods = [
   {
@@ -37,13 +39,62 @@ var selectedIndicator, selectedYear;
 var colorMin = colorbrewer.Greens[3][0];
 var colorMax = colorbrewer.Greens[3][2];
 
-var countries, codes={}, legend;
+var countries, codes={}, countryDetails={};
+
 var path = d3.geo.path().projection(projectionMethods[0].method);
 
-function runAQueryOn(indicator, year) {
+var updateTextLabel = function(country) {
+  var detailHtml = "";
+  if (country != "") {
+    var details = countryDetails[country];
+    var value = countries.reduce(function(prev, curr) {
+      if (codes[curr.id] == country) return curr.value;
+      return prev;
+    }, NaN);
+
+    detailHtml = "<h1>" + details.name + "</h1>";
+
+    if (!isNaN(value)) detailHtml += "<h2>Value: " + Math.round(value).toLocaleString() + "</h2>";
+
+    detailHtml +=
+      "<table><tbody>" +
+      "  <tr><th>Region:</th><td>" + details.region.value + "</td>" +
+      "  <tr><th>Latitude:</th><td>" + details.latitude + "</td>" +
+      "  <tr><th>Longitude:</th><td>" + details.longitude + "</td>" +
+      "  <tr><th>Capital:</th><td>" + details.capitalCity + "</td>" +
+      "  <tr><th>Income Level:</th><td>" + details.incomeLevel.value + "</td>" +
+      "  <tr><th>Lending Type:</th><td>" + details.lendingType.value + "</td>" +
+      "</tbody></table>"
+  }
+
+  textLabel.html(detailHtml);
+}
+
+function queryCountry(country) {
+  // use a cached result if we have it
+  if (countryDetails[country]) {
+    updateTextLabel(country);
+  } else {
+    $.ajax({
+      url: "http://api.worldbank.org/countries/" + country + "?format=jsonP&prefix=getcountry&per_page=500",
+      jsonpCallback: 'getcountry',
+      dataType: 'jsonp',
+      success: function (data, status){
+        if (data[1]) {
+          countryDetails[country] = data[1][0];
+          updateTextLabel(country);
+        } else {
+          updateTextLabel("");
+        }
+      }
+    });
+  }
+}
+
+function queryIndicator(indicator, year) {
   $.ajax({
-    url: "http://api.worldbank.org/countries/all/indicators/" + indicator + "?format=jsonP&prefix=getdata&per_page=500&date=" + year + ":" + year,
-    jsonpCallback: 'getdata',
+    url: "http://api.worldbank.org/countries/all/indicators/" + indicator + "?format=jsonP&prefix=getindicator&per_page=500&date=" + year + ":" + year,
+    jsonpCallback: 'getindicator',
     dataType: 'jsonp',
     success: function (data, status){
       // transform data
@@ -69,15 +120,18 @@ function runAQueryOn(indicator, year) {
       var scale = d3.scale.linear().domain(dataSetExtent).range([colorMin, colorMax]);
 
       // update legend labels
-      d3.select("#minVal").text(Math.round(dataSetExtent[0]));
-      d3.select("#maxVal").text(Math.round(dataSetExtent[1]));
+      d3.select("#minVal").text(Math.round(dataSetExtent[0]).toLocaleString());
+      d3.select("#maxVal").text(Math.round(dataSetExtent[1]).toLocaleString());
+
+      // update details
+      updateTextLabel("");
 
       // update graph colors
       svg.selectAll(".country")
         .data(countries)
         .transition().duration(750)
           .attr("d", path)
-          .style("fill", function(d) { if (d.value) return scale(d.value); });
+          .style("fill", function(d) { if (d.value) return scale(d.value); return "lightgray"; });
     }
   });
 }
@@ -117,11 +171,11 @@ var initVis = function(error, indicators, world, country_codes) {
       .attr({
         class: "country",
         d: path
-      });
+      })
+      .style("fill", "lightgray")
+      .on("click", countryClicked);
 
-  // create legend
-  legend = svg.append("g");
-
+  // configure legend
   gradient = legend.append("defs").append("linearGradient")
     .attr({
       id: "gradient",
@@ -178,29 +232,32 @@ queue()
   // the following data is from: https://gist.github.com/jeremybuis/4997305
   // and from: http://www.freeformatter.com/iso-country-list-html-select.html
   .defer(d3.json,"../data/wikipedia-iso-country-codes.json")
-  // .defer(d3.json,"../data/WorldBankCountries.json")
   .await(initVis);
 
-var textLabel = svg.append("text").text(projectionMethods[actualProjectionMethod].name).attr({
+var projectionLabel = svg.append("text").text(projectionMethods[actualProjectionMethod].name).attr({
   transform: "translate(-40,-30)"
 })
+
+var countryClicked = function(d) {
+  queryCountry(codes[d.id]);
+}
 
 var changeIndicator = function() {
   selectedIndicator = d3.event.target.value;
   console.log("indicator:", selectedIndicator);
-  runAQueryOn(selectedIndicator, selectedYear);
+  queryIndicator(selectedIndicator, selectedYear);
 }
 
 var changeYear = function() {
   selectedYear = d3.event.target.value;
   console.log("year:", selectedYear);
-  runAQueryOn(selectedIndicator, selectedYear);
+  queryIndicator(selectedIndicator, selectedYear);
 }
 
 var changePro = function() {
   actualProjectionMethod = (actualProjectionMethod+1) % (projectionMethods.length);
 
-  textLabel.text(projectionMethods[actualProjectionMethod].name);
+  projectionLabel.text(projectionMethods[actualProjectionMethod].name);
   path = d3.geo.path().projection(projectionMethods[actualProjectionMethod].method);
   svg.selectAll(".country")
     .transition().duration(750)
